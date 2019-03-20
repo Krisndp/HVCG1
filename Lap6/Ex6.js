2db23d68ee0aa38554028e15376b9562943563e6
DucThang6419 <thangtran.6419@gmail.com>
2019-03-20 16:53:28 +0700

    lap6

---
+++ Lap6/Ex6.js
@@ -0,0 +1,82 @@
+// // JavaScript Document
+// function validateForm() {
+// //Họ phải được điền
+// var ho = document.forms[“myForm”][“ho”].value;
+// if (ho == “”) {
+// alert(“Họ không được để trống”);
+// return false;
+// }
+// //Tên phải được điền
+// var ten = document.forms[“myForm”][“ten”].value;
+// if (ten == “”) {
+// alert(“Tên không được để trống”);
+// return false;
+// }
+// //Email phải được điền chính xác
+// var email=document.forms[“myForm”][“email”].value;
+// var aCong=email.indexOf(“@”);
+// var dauCham = email.lastIndexOf(“.”);
+// if (email == “”) {
+// alert(“Email không được để trống”);
+// return false;
+// }
+// else if ((aCong<1) || (dauCham<aCong+2) || (dauCham+2>email.length)) {
+// alert(“Email bạn điền không chính xác”);
+// return false;
+// }
+// //Nhập số điện thoại
+// var dienThoai = document.forms[“myForm”][“dienThoai”].value;
+// var kiemTraDT = isNaN(dienThoai);
+// if (dienThoai == “”) {
+// alert(“Điện thoại không được để trống”);
+// return false;
+// }
+// if (kiemTraDT == true) {
+// alert(“Điện thoại phải để ở định dạng số”);
+// return false;
+// }
+// //Nhập số lượng muốn mua
+// var soLuong = document.forms[“myForm”][“soLuong”].value;
+// var kiemTraSL = isNaN(soLuong);
+// if ((soLuong == “”) || (soLuong <= 0)) {
+// alert(“Số lượng không được để trống và phải lớn hơn 0”);
+// return false;
+// }
+// if (soLuong > 100) {
+// alert(“Số lượng mua không được lớn hơn 100”);
+// return false;
+// }
+// if (kiemTraSL == true) {
+// alert(“Số lượng mua phải để ở định dạng số”);
+// return false;
+// }
+// //Chọn ngày nhận hàng
+// var nm = document.forms[“myForm”][“ngaymua”].value;
+// if (nm == “”) {
+// alert(“Ngày không được để trống”);
+// return false;
+// }
+// //Chọn kiểu thanh toán
+// var ck = document.getElementById(“ck”);
+// var tt = document.getElementById(“tt”);
+// if ((ck.checked == false) && (tt.checked == false)) {
+// alert(“Bạn phải chọn một kiểu thanh toán”);
+// return false;
+// 
+// }
+
+
+function Check(){
+
+	if(document.getElementById('name').value == '' || document.getElementById('name').value == null ){
+		alert('Họ tên phải bắt buộc');
+	} else if ( document.getElementById('class').value == "" || document.getElementById('class').value == null){
+		alert('Lớp phải bắt buộc');
+	} else if (document.getElementById('math').value < 0 || document.getElementById('math').value > 10 || isNaN(document.getElementById('math').value) == true || document.getElementById('math').value == ''){
+		alert('Điểm Toán phải nhập và phải là số từ 0 đến 10 ')
+	} else if (document.getElementById('chem').value < 0 || document.getElementById('chem').value > 10 || isNaN(document.getElementById('chem').value) == true || document.getElementById('chem').value == ''){
+		alert('Điểm Hóa phải nhập và phải là số từ 0 đến 10 ')
+	} else if (document.getElementById('phys').value < 0 || document.getElementById('phys').value > 10 || isNaN(document.getElementById('phys').value) == true || document.getElementById('phys').value == ''){
+		alert('Điểm Lý phải nhập và phải là số từ 0 đến 10 ')
+	}
+}
